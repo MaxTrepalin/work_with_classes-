c104378eb28634a520f8aa1dc4920538034e9bae
Max_Trepalin <74497851+MaxTrepalin@users.noreply.github.com>
2021-02-04 21:26:57 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <string>
+#include <fstream>
+
+#include "Date.h"
+#include "Adress.h"
+#include "Phone.h"
+#include "Person.h"
+
+using namespace std;
+
+int main() {
+
+    Date birth;
+    Date now(2021, 2, 4);
+
+}
